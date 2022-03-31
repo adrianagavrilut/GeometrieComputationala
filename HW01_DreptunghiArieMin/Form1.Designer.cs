088e6db96b562fbec0969ed86d46a15101ac388d
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-03-31 22:40:09 +0300

    add HW01

---
+++ HW01_DreptunghiArieMin/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace HW01_DreptunghiArieMin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.buttonCompute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 400);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(808, 12);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(64, 23);
+            this.buttonDraw.TabIndex = 1;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // buttonCompute
+            // 
+            this.buttonCompute.Location = new System.Drawing.Point(808, 42);
+            this.buttonCompute.Name = "buttonCompute";
+            this.buttonCompute.Size = new System.Drawing.Size(64, 23);
+            this.buttonCompute.TabIndex = 2;
+            this.buttonCompute.Text = "Compute";
+            this.buttonCompute.UseVisualStyleBackColor = true;
+            this.buttonCompute.Click += new System.EventHandler(this.buttonCompute_Click);
+            // 
+            // Puncte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.buttonCompute);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.panel1);
+            this.Name = "Puncte";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button buttonCompute;
+    }
+}
+
---
+++ HW01_DreptunghiArieMin/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW01_DreptunghiArieMin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        Graphics g;
+        int n, xMax = 0, yMax = 0, xMin = 900, yMin = 500;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+
+            Pen p = new Pen(Color.Black, 3);
+            n = rnd.Next(500);
+            for (int i = 0; i < n; i++)
+            {
+                int x = rnd.Next(80, 800);
+                int y = rnd.Next(80, 400);
+                g.DrawEllipse(p, x, y, 3, 3);
+                if (xMax < x)
+                {
+                    xMax = x;
+                }
+                if (yMax < y)
+                {
+                    yMax = y;
+                }
+                if (xMin > x)
+                {
+                    xMin = x;
+                }
+                if (yMin > y)
+                {
+                    yMin = y;
+                }
+            }
+        }
+
+        private void buttonCompute_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen dr = new Pen(Color.Red, 3);
+            g.DrawLine(dr, xMin, yMin, xMax, yMin); //lungime sus
+            g.DrawLine(dr, xMin, yMax, xMax, yMax); //lungime jos
+            g.DrawLine(dr, xMin, yMin, xMin, yMax); //latime stanga
+            g.DrawLine(dr, xMax, yMin, xMax, yMax); //latime dreapta
+        }
+    }
+}
