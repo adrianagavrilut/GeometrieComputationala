14ee9169618dc792e0e0a0a2ff30db47ef19fed0
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-03-31 22:34:57 +0300

    Add project files.

---
+++ 03HW_CercArieMin/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace _03HW_CercArieMin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.buttonCompute = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(740, 380);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(796, 120);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 0;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(758, 12);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(73, 20);
+            this.textBoxN.TabIndex = 1;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(758, 38);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(73, 26);
+            this.buttonDraw.TabIndex = 2;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // buttonCompute
+            // 
+            this.buttonCompute.Location = new System.Drawing.Point(758, 70);
+            this.buttonCompute.Name = "buttonCompute";
+            this.buttonCompute.Size = new System.Drawing.Size(73, 23);
+            this.buttonCompute.TabIndex = 3;
+            this.buttonCompute.Text = "Compute";
+            this.buttonCompute.UseVisualStyleBackColor = true;
+            this.buttonCompute.Click += new System.EventHandler(this.buttonCompute_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(758, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Aria Minima";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 434);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonCompute);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Triunghi Arie Minima";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button buttonCompute;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 03HW_CercArieMin/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03HW_CercArieMin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int n;
+        Graphics g;
+        List<Point> points;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(SystemColors.GradientInactiveCaption);
+            Brush brush = new SolidBrush(Color.Navy);
+            if (!int.TryParse(this.textBoxN.Text, out n))
+            {
+                n = 0;
+                MessageBox.Show("Va rugam sa introduceti un numar");
+            }
+            points = new List<Point>();
+            for (int i = 0; i < n; i++)
+            {
+                Point p = new Point(rnd.Next(200, this.panel1.Width - 200), rnd.Next(100, this.panel1.Height - 100));
+                points.Add(p);
+                g.FillEllipse(brush, p.X - 2, p.Y - 2, 4, 4);
+                // deseneaza cercurile
+            }
+        }
+
+        private void buttonCompute_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen cerc = new Pen(Color.Red, 2);
+            double distMAx = Double.MinValue;
+            double distMAx1 = Double.MinValue;
+            float x1 = 0, x2 = 0, y1 = 0, y2 = 0, xA = 0, yA = 0;
+            //distanta cea mai mare dintre doua puncte
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = i + 1; j < points.Count - 1; j++)
+                {
+                    double diametru = Distanta2Pct(points[i].X, points[i].Y, points[j].X, points[j].Y);
+                    if (distMAx < diametru)
+                    {
+                        distMAx = diametru;
+                        //coordonatele celor mai deparate doua puncte
+                        x1 = points[i].X;
+                        x2 = points[j].X;
+                        y1 = points[i].Y;
+                        y2 = points[j].Y;
+                    }
+                }
+            }
+            //coordonate centru
+            float xCentru = (float)(x1 + x2) / 2;
+            float yCentru = (float)(y1 + y2) / 2;
+            for (int i = 0; i < points.Count; i++)
+            {
+                float dist1 = Distanta2Pct(points[i].X, points[i].Y, xCentru, yCentru);
+                if (distMAx1 < dist1)
+                {
+                    distMAx1 = dist1;
+                    //punctul A cel mai departat de centru
+                    xA = points[i].X;
+                    yA = points[i].Y;
+                }
+            }
+            //raza cercului e distanta dintre punctul A si centru
+            float raza = Distanta2Pct(xCentru, yCentru, xA, yA);
+            //deseneaza cercul
+            g.DrawEllipse(cerc, xCentru - raza, yCentru - raza, raza + raza, raza + raza);
+        }
+
+        private float Distanta2Pct(float x1, float y1, float x2, float y2)
+        {
+            float dist = (float)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return dist;
+        }
+    }
+}
