2e38b7f372711353974fe7174503cb0af2090fcc
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-04-02 17:12:45 +0300

    add HW05 Jarvis

---
+++ HW05_ConvexHull_AlgJarvis/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace HW05_ConvexHull_AlgJarvis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.buttonCompute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Location = new System.Drawing.Point(11, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(732, 445);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(750, 10);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(75, 20);
+            this.textBoxN.TabIndex = 1;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(750, 37);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(75, 23);
+            this.buttonDraw.TabIndex = 2;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click_1);
+            // 
+            // buttonCompute
+            // 
+            this.buttonCompute.Location = new System.Drawing.Point(750, 67);
+            this.buttonCompute.Name = "buttonCompute";
+            this.buttonCompute.Size = new System.Drawing.Size(75, 23);
+            this.buttonCompute.TabIndex = 3;
+            this.buttonCompute.Text = "Compute";
+            this.buttonCompute.UseVisualStyleBackColor = true;
+            this.buttonCompute.Click += new System.EventHandler(this.buttonCompute_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 471);
+            this.Controls.Add(this.buttonCompute);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "ConvexHullJarvis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button buttonCompute;
+    }
+}
+
---
+++ HW05_ConvexHull_AlgJarvis/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW05_ConvexHull_AlgJarvis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int n;
+        Graphics g;
+        List<Point> points;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+        }
+
+        private void buttonDraw_Click_1(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(SystemColors.GradientInactiveCaption);
+            Brush brush = new SolidBrush(Color.Black);
+            if (!int.TryParse(this.textBoxN.Text, out n))
+            {
+                n = 0;
+                MessageBox.Show("Va rugam sa introduceti un numar");
+            }
+            points = new List<Point>();
+            for (int i = 0; i < n; i++)
+            {
+                Point p = new Point(rnd.Next(50, this.panel1.Width - 50), rnd.Next(50, this.panel1.Height - 50));
+                points.Add(p);
+                g.FillEllipse(brush, p.X - 2, p.Y - 2, 5, 5); // deseneaza cercurile
+
+            }
+        }
+
+        private void buttonCompute_Click_1(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen convex = new Pen(Color.DarkMagenta, 3);
+
+            List<int> hull = new List<int>(); //lista indicilor pct invelitorii convexe
+            bool goOn;
+            int iMin = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (points[i].Y < points[iMin].Y)
+                {
+                    iMin = i;
+                }
+            }
+            hull.Add(iMin);//punctul cel mai de jos si din stanga este primul punct din hull
+            do
+            {
+                goOn = true;
+                int pArbitrar = (hull[hull.Count - 1] + 1) % n;//un punct aleator care sa fie difeit de ultimul pct de pe invelitoare( %n ca sa nu treaca de n, ci sa apartina listei)
+                for (int i = 0; i < n; i++)
+                {
+                    //sensul trig intre ultimul punct dun hull, fiecare punct din lista si un punct aleator
+                    if (DetSensTrigonometric(points[hull[hull.Count - 1]].X, points[hull[hull.Count - 1]].Y, points[i].X, points[i].Y, points[pArbitrar].X, points[pArbitrar].Y) > 0)
+                    {
+                        pArbitrar = i;
+                    }
+                }
+                hull.Add(pArbitrar); //se adauga urmatorul pct de pe invelitoare
+                if (pArbitrar == iMin)
+                {
+                    goOn = false;//daca ajunge la primul punct se opreste
+                }
+            } while (goOn);
+
+            for (int i = 0; i < hull.Count - 1; i++)
+            {
+                //desenarea invelitorii convexe
+                g.DrawLine(convex, points[hull[i]].X, points[hull[i]].Y, points[hull[i+1]].X, points[hull[i+1]].Y);
+            }
+        }
+
+        private double DetSensTrigonometric(double pX, double pY, double qX, double qY, double rX, double rY)
+        {
+            return pX * qY + qX * rY + pY * rX - rX * qY - pX * rY - qX * pY;
+            // det = 0 => coliniare
+            // det > 0 => sensul invers trig, sensul acelor de ceasornic, 'spre dreapta'
+            // det < 0 => sensul trigonometric, 'spre stanga'
+        }
+    }
+}
