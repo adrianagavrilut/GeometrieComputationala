ef6079b319c6db8489ca2b7e5cc6a250b870b919
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-04-06 09:53:46 +0300

    add HW06 initial

---
+++ HW06_DrawPolygon/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW06_DrawPolygon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Point> points = new List<Point>();
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Point p = panel1.PointToClient(Cursor.Position);
+            points.Add(p);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
