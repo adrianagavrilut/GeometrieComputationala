71a3b4e7749fbbc762faa339ff91e05fe5671d8b
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-04-11 23:28:04 +0300

    add HW06 solution

--- HW06_DrawPolygon/Form1.Designer.cs
+++ HW06_DrawPolygon/Form1.Designer.cs
@@ -30,44 +30,59 @@ namespace HW06_DrawPolygon
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonDrawingMode = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(697, 341);
             this.panel1.TabIndex = 0;
-            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
-            // button1
+            // buttonDrawingMode
             // 
-            this.button1.Location = new System.Drawing.Point(716, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(53, 35);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonDrawingMode.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.buttonDrawingMode.Location = new System.Drawing.Point(716, 13);
+            this.buttonDrawingMode.Name = "buttonDrawingMode";
+            this.buttonDrawingMode.Size = new System.Drawing.Size(53, 35);
+            this.buttonDrawingMode.TabIndex = 1;
+            this.buttonDrawingMode.Text = "Draw";
+            this.buttonDrawingMode.UseVisualStyleBackColor = false;
+            this.buttonDrawingMode.Click += new System.EventHandler(this.buttonDrawingMode_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(716, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(781, 365);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(783, 406);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDrawingMode);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1DrawPolygon";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonDrawingMode;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- HW06_DrawPolygon/Form1.cs
+++ HW06_DrawPolygon/Form1.cs
@@ -17,17 +17,101 @@ namespace HW06_DrawPolygon
             InitializeComponent();
         }
 
+        Graphics g;
         List<Point> points = new List<Point>();
+        int contor = 1;
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //List<List<Point>> polygons = new List<List<Point>>();
+        //int contorPoligoane = -1;
+        //bool drawingMode = false;
 
-        private void panel1_Click(object sender, EventArgs e)
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            Point p = panel1.PointToClient(Cursor.Position);
-            points.Add(p);
+            g = e.Graphics;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
         {
-            
+            Graphics g = panel1.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 3);
+            Point aux = new Point(e.X, e.Y);
+            g.DrawString(contor.ToString(), new Font(FontFamily.GenericSansSerif, 10), new SolidBrush(Color.Black), aux.X - 20, aux.Y - 20);
+            contor++;
+            g.DrawEllipse(pen, aux.X - 2, aux.Y - 2, 4, 4);
+            Pen linie = new Pen(Color.DarkViolet, 2);
+            if (points.Count != 0)
+            {
+                g.DrawLine(linie, aux, points[points.Count - 1]);
+            }
+            points.Add(aux);
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //if (drawingMode)
+            //{
+            //    Pen pen = new Pen(Color.Black, 3);
+            //    Point aux = new Point(e.X, e.Y);
+            //    Pen linie = new Pen(Color.DarkViolet, 2);
+            //    if (polygons[contorPoligoane].Count != 0)
+            //    {
+            //        if ((aux.X >= polygons[contorPoligoane][0].X - 2 && aux.X <= (polygons[contorPoligoane][0].X + 2)) && (aux.Y >= polygons[contorPoligoane][0].Y - 2 && aux.Y <= (polygons[contorPoligoane][0].Y + 2)))
+            //        {
+            //            drawingMode = false;
+            //            buttonDrawingMode.BackColor = Color.Red;
+            //        }
+            //        if (drawingMode)
+            //        {
+            //            g.DrawString(contor.ToString(), new Font(FontFamily.GenericSansSerif, 10), new SolidBrush(Color.Black), aux.X - 20, aux.Y - 20);
+            //            contor++;
+            //            g.DrawEllipse(pen, aux.X - 2, aux.Y - 2, 4, 4);
+            //            g.DrawLine(linie, aux, polygons[contorPoligoane][polygons[contorPoligoane].Count - 1]);
+            //            polygons[contorPoligoane].Add(aux);
+            //        }
+            //        else
+            //        {
+            //            g.DrawLine(linie, polygons[contorPoligoane][0], polygons[contorPoligoane][polygons[contorPoligoane].Count - 1]);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        g.DrawString(contor.ToString(), new Font(FontFamily.GenericSansSerif, 10), new SolidBrush(Color.Black), aux.X - 20, aux.Y - 20);
+            //        contor++;
+            //        label1.Text = (contorPoligoane + 1).ToString();
+            //        g.DrawEllipse(pen, aux.X - 2, aux.Y - 2, 4, 4);
+            //        polygons[contorPoligoane].Add(aux);
+
+            //    }
+            //}
+        }
+
+        private void buttonDrawingMode_Click(object sender, EventArgs e)
+        {
+        //    if (!drawingMode)
+        //    {
+        //        List<Point> points = new List<Point>();
+        //        polygons.Add(points);
+        //        contor = 1;
+        //        contorPoligoane++;
+        //        drawingMode = true;
+        //        buttonDrawingMode.BackColor = Color.LightCyan;
+        //    }
+        //    else
+        //    {
+        //        Graphics g = panel1.CreateGraphics();
+        //        Pen linie = new Pen(Color.DarkViolet, 2);
+        //        if (polygons[contorPoligoane].Count != 0)
+        //        {
+        //            g.DrawLine(linie, polygons[contorPoligoane][0], polygons[contorPoligoane][polygons[contorPoligoane].Count - 1]);
+        //            Brush brush = Brushes.LightGoldenrodYellow;
+        //            g.FillPolygon(brush, polygons[contorPoligoane].ToArray());
+        //        }
+        //        else
+        //        {
+        //            polygons.RemoveAt(contorPoligoane);
+        //            contorPoligoane--;
+        //        }
+        //        drawingMode = false;
+        //        buttonDrawingMode.BackColor = Color.LightSkyBlue;
+        //    }
+
         }
     }
 }
