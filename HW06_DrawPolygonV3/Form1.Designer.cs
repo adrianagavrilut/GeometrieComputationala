5c395a3137437db3d08a2f7ebe1e77fdf0048767
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-04-18 22:39:31 +0300

    add final form of Draw Polygon v3

--- HW06_DrawPolygonV3/Form1.Designer.cs
+++ HW06_DrawPolygonV3/Form1.Designer.cs
@@ -31,8 +31,7 @@ namespace HW06_DrawPolygonV3
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.buttonFinishUp = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -55,37 +54,28 @@ namespace HW06_DrawPolygonV3
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // buttonFinishUp
             // 
-            this.button2.Location = new System.Drawing.Point(639, 43);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "FinishUp";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(639, 73);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.buttonFinishUp.Enabled = false;
+            this.buttonFinishUp.Location = new System.Drawing.Point(639, 43);
+            this.buttonFinishUp.Name = "buttonFinishUp";
+            this.buttonFinishUp.Size = new System.Drawing.Size(75, 23);
+            this.buttonFinishUp.TabIndex = 2;
+            this.buttonFinishUp.Text = "FinishUp";
+            this.buttonFinishUp.UseVisualStyleBackColor = true;
+            this.buttonFinishUp.Click += new System.EventHandler(this.buttonFinishUp_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(740, 367);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonFinishUp);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1DrawPolyV3";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -93,8 +83,7 @@ namespace HW06_DrawPolygonV3
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonFinishUp;
     }
 }
 
--- HW06_DrawPolygonV3/Form1.cs
+++ HW06_DrawPolygonV3/Form1.cs
@@ -20,8 +20,6 @@ namespace HW06_DrawPolygonV3
         Graphics g;
         List<Point> points = new List<Point>();
         int contor = 1;
-        List<List<Point>> polygons = new List<List<Point>>();
-        int contorPoligoane = -1;
         bool drawingMode = false;
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -37,35 +35,14 @@ namespace HW06_DrawPolygonV3
                 Pen pen = new Pen(Color.Black, 3);
                 Point aux = new Point(e.X, e.Y);
                 Pen linie = new Pen(Color.DarkViolet, 2);
-                if (polygons[contorPoligoane].Count != 0)
+                g.DrawString(contor.ToString(), new Font(FontFamily.GenericSansSerif, 10), new SolidBrush(Color.Black), aux.X - 20, aux.Y - 20);
+                contor++;
+                g.DrawEllipse(pen, aux.X - 2, aux.Y - 2, 4, 4);
+                if (points.Count != 0)
                 {
-                    if ((aux.X >= polygons[contorPoligoane][0].X - 4 && aux.X <= (polygons[contorPoligoane][0].X + 4)) && (aux.Y >= polygons[contorPoligoane][0].Y - 4 && aux.Y <= (polygons[contorPoligoane][0].Y + 4)))
-                    {
-                        drawingMode = false;
-                        button1.BackColor = Color.LightSkyBlue;
-                    }
-                    if (drawingMode)
-                    {
-                        g.DrawString(contor.ToString(), new Font(FontFamily.GenericSansSerif, 10), new SolidBrush(Color.Black), aux.X - 20, aux.Y - 20);
-                        contor++;
-                        g.DrawEllipse(pen, aux.X - 2, aux.Y - 2, 4, 4);
-                        g.DrawLine(linie, aux, polygons[contorPoligoane][polygons[contorPoligoane].Count - 1]);
-                        polygons[contorPoligoane].Add(aux);
-                    }
-                    else
-                    {
-                        g.DrawLine(linie, polygons[contorPoligoane][0], polygons[contorPoligoane][polygons[contorPoligoane].Count - 1]);
-                    }
+                    g.DrawLine(linie, aux, points[points.Count - 1]);
                 }
-                else
-                {
-                    g.DrawString(contor.ToString(), new Font(FontFamily.GenericSansSerif, 10), new SolidBrush(Color.Black), aux.X - 20, aux.Y - 20);
-                    contor++;
-                    label1.Text = (contorPoligoane + 1).ToString();
-                    g.DrawEllipse(pen, aux.X - 2, aux.Y - 2, 4, 4);
-                    polygons[contorPoligoane].Add(aux);
-
-                }
+                points.Add(aux);
             }
         }
 
@@ -73,30 +50,21 @@ namespace HW06_DrawPolygonV3
         {
             if (!drawingMode)
             {
-                List<Point> points = new List<Point>();
-                polygons.Add(points);
-                contor = 1;
-                contorPoligoane++;
                 drawingMode = true;
-                button1.BackColor = Color.LightCyan;
+                button1.Enabled = false;
+                buttonFinishUp.Enabled = true;
             }
-            else
+        }
+
+        private void buttonFinishUp_Click(object sender, EventArgs e)
+        {
+            if(drawingMode)
             {
                 Graphics g = panel1.CreateGraphics();
                 Pen linie = new Pen(Color.DarkViolet, 2);
-                if (polygons[contorPoligoane].Count != 0)
-                {
-                    g.DrawLine(linie, polygons[contorPoligoane][0], polygons[contorPoligoane][polygons[contorPoligoane].Count - 1]);
-                    //Brush brush = Brushes.LightGoldenrodYellow;
-                    //g.FillPolygon(brush, polygons[contorPoligoane].ToArray());
-                }
-                else
-                {
-                    polygons.RemoveAt(contorPoligoane);
-                    contorPoligoane--;
-                }
+                g.DrawLine(linie, points[0], points[points.Count - 1]);
+                buttonFinishUp.Enabled = false;
                 drawingMode = false;
-                button1.BackColor = Color.LightSkyBlue;
             }
         }
     }
