83ac6b0a602f2f8d4a638d0ef66934989c06035f
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-04-27 18:44:58 +0300

    add initial status - drawing polygon

--- HW07_Triangulare_Diagonale/Form1.cs
+++ HW07_Triangulare_Diagonale/Form1.cs
@@ -16,5 +16,56 @@ namespace HW07_Triangulare_Diagonale
         {
             InitializeComponent();
         }
+
+        Graphics g;
+        List<Point> points = new List<Point>();
+        int contor = 1;
+        bool drawingMode = false;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            if (drawingMode)
+            {
+                Pen pen = new Pen(Color.Black, 3);
+                Point aux = new Point(e.X, e.Y);
+                Pen linie = new Pen(Color.DarkViolet, 2);
+                g.DrawString(contor.ToString(), new Font(FontFamily.GenericSansSerif, 10), new SolidBrush(Color.Black), aux.X - 20, aux.Y - 20);
+                contor++;
+                g.DrawEllipse(pen, aux.X - 2, aux.Y - 2, 4, 4);
+                if (points.Count != 0)
+                {
+                    g.DrawLine(linie, aux, points[points.Count - 1]);
+                }
+                points.Add(aux);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!drawingMode)
+            {
+                drawingMode = true;
+                button1.Enabled = false;
+                buttonFinishUp.Enabled = true;
+            }
+        }
+
+        private void buttonFinishUp_Click(object sender, EventArgs e)
+        {
+            if (drawingMode)
+            {
+                Graphics g = panel1.CreateGraphics();
+                Pen linie = new Pen(Color.DarkViolet, 2);
+                g.DrawLine(linie, points[0], points[points.Count - 1]);
+                buttonFinishUp.Enabled = false;
+                drawingMode = false;
+            }
+        }
     }
 }
