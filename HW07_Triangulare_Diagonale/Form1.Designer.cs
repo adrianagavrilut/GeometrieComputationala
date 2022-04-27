55bd4565ec8aa42109be56eb46d2753a5e57d523
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-04-27 23:23:03 +0300

    add triangulare with errors

--- HW07_Triangulare_Diagonale/Form1.Designer.cs
+++ HW07_Triangulare_Diagonale/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace HW07_Triangulare_Diagonale
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.buttonFinishUp = new System.Windows.Forms.Button();
+            this.buttonTriang = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -65,11 +66,23 @@ namespace HW07_Triangulare_Diagonale
             this.buttonFinishUp.UseVisualStyleBackColor = true;
             this.buttonFinishUp.Click += new System.EventHandler(this.buttonFinishUp_Click);
             // 
+            // buttonTriang
+            // 
+            this.buttonTriang.Enabled = false;
+            this.buttonTriang.Location = new System.Drawing.Point(639, 73);
+            this.buttonTriang.Name = "buttonTriang";
+            this.buttonTriang.Size = new System.Drawing.Size(75, 23);
+            this.buttonTriang.TabIndex = 3;
+            this.buttonTriang.Text = "Triangulate";
+            this.buttonTriang.UseVisualStyleBackColor = true;
+            this.buttonTriang.Click += new System.EventHandler(this.buttonTriang_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(740, 367);
+            this.Controls.Add(this.buttonTriang);
             this.Controls.Add(this.buttonFinishUp);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
@@ -84,6 +97,7 @@ namespace HW07_Triangulare_Diagonale
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button buttonFinishUp;
+        private System.Windows.Forms.Button buttonTriang;
     }
 }
 
--- HW07_Triangulare_Diagonale/Form1.cs
+++ HW07_Triangulare_Diagonale/Form1.cs
@@ -65,7 +65,109 @@ namespace HW07_Triangulare_Diagonale
                 g.DrawLine(linie, points[0], points[points.Count - 1]);
                 buttonFinishUp.Enabled = false;
                 drawingMode = false;
+                buttonTriang.Enabled = true;
             }
+        }
+
+        List<int> diagonale = new List<int>();
+
+        private void buttonTriang_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen penTR = new Pen(Color.Purple, 2);
+            int contorDiag = 0;
+            for (int i = 0; i < points.Count - 3; i++)
+            {
+                for (int j = i + 2; j < points.Count - 1; j++)
+                {
+                    //g.DrawLine(penTR, points[i], points[j]);
+
+                    if (i == 0 && j == points.Count - 1)
+                    {
+                        break;
+                    }
+                    if (!IntersectieLaturi(points[i], points[j], points[i + 1], points[j + 1]) && !IntersectieLaturi(points[i], points[j], points[i - 1], points[j - 1]) &&
+                        !IntersectieDiagonale(points[i], points[j]) &&
+                        InteriorPoligon(points[i], points[j]))
+                    {
+                        diagonale.Add(i);
+                        diagonale.Add(j);
+                        g.DrawLine(penTR, points[i], points[j]);
+                        contorDiag++;
+                    }
+                    if (contorDiag == points.Count - 3)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private bool IntersectieLaturi(Point a1, Point b1, Point a2, Point b2)
+        {
+            if (ProdusDeterminant(b2, b1, a1, a2) <= 0 && ProdusDeterminant(a2, a1, b1, b2) <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private int ProdusDeterminant(Point b2, Point b1, Point a1, Point a2)
+        {
+            return ValoareDeterminant(b2, b1, a1) * ValoareDeterminant(b2, b1, a2);
+        }
+
+        private int ValoareDeterminant(Point a, Point b , Point c)
+        {
+            return a.X * b.Y + b.X * c.Y + c.X * a.Y - c.X * b.Y - a.X * c.Y - b.X * a.Y;
+        }
+
+        private bool IntersectieDiagonale(Point a, Point b)
+        {
+            for (int i = 0; i < diagonale.Count - 1; i++)
+            {
+                if (ProdusDeterminant(b, a, points[diagonale[i]], points[diagonale[i + 1]]) <= 0 && ProdusDeterminant(points[diagonale[i + 1]], points[diagonale[i]], a, b) <= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool InteriorPoligon(Point a, Point b)
+        {
+            int iSus = -1, iJos = -1, iSt = -1, iDr = -1;
+            //cel mai de sus pct
+            //cel mai de jos
+            //cel mai din stanga
+            //cel mai din drapta
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (iSus < points[i].Y)
+                {
+                    iSus = i;
+                }
+                if (iJos > points[i].Y)
+                {
+                    iJos = i;
+                }
+                if (iSt < points[i].X)
+                {
+                    iSt = i;
+                }
+                if (iDr > points[i].X)
+                {
+                    iDr = i;
+                }
+            }
+            //a, b in interior
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (a.Y > points[iSus].Y && a.Y < points[iJos].Y && a.X > points[iSt].X && a.X < points[iDr].Y && b.Y > points[iSus].Y && b.Y < points[iJos].Y && b.X > points[iSt].X && b.X < points[iDr].Y)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
