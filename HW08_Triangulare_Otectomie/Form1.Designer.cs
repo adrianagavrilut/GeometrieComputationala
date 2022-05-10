dbdde9215bcf1c7ca90638be22838d7815e21f83
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-05-11 00:58:34 +0300

    add HW08 final form

--- HW08_Triangulare_Otectomie/Form1.Designer.cs
+++ HW08_Triangulare_Otectomie/Form1.Designer.cs
@@ -29,13 +29,143 @@ namespace HW08_Triangulare_Otectomie
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonDrawMode = new System.Windows.Forms.Button();
+            this.buttonFinishUp = new System.Windows.Forms.Button();
+            this.buttonTriang = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonThreeColor = new System.Windows.Forms.Button();
+            this.buttonArie = new System.Windows.Forms.Button();
+            this.labelArie = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(655, 363);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // buttonDrawMode
+            // 
+            this.buttonDrawMode.Location = new System.Drawing.Point(762, 12);
+            this.buttonDrawMode.Name = "buttonDrawMode";
+            this.buttonDrawMode.Size = new System.Drawing.Size(75, 25);
+            this.buttonDrawMode.TabIndex = 1;
+            this.buttonDrawMode.Text = "DrawMode";
+            this.buttonDrawMode.UseVisualStyleBackColor = true;
+            this.buttonDrawMode.Click += new System.EventHandler(this.buttonDrawMode_Click);
+            // 
+            // buttonFinishUp
+            // 
+            this.buttonFinishUp.Enabled = false;
+            this.buttonFinishUp.Location = new System.Drawing.Point(762, 43);
+            this.buttonFinishUp.Name = "buttonFinishUp";
+            this.buttonFinishUp.Size = new System.Drawing.Size(75, 25);
+            this.buttonFinishUp.TabIndex = 2;
+            this.buttonFinishUp.Text = "FinishUp";
+            this.buttonFinishUp.UseVisualStyleBackColor = true;
+            this.buttonFinishUp.Click += new System.EventHandler(this.buttonFinishUp_Click);
+            // 
+            // buttonTriang
+            // 
+            this.buttonTriang.Enabled = false;
+            this.buttonTriang.Location = new System.Drawing.Point(762, 74);
+            this.buttonTriang.Name = "buttonTriang";
+            this.buttonTriang.Size = new System.Drawing.Size(75, 25);
+            this.buttonTriang.TabIndex = 3;
+            this.buttonTriang.Text = "Triangulate";
+            this.buttonTriang.UseVisualStyleBackColor = true;
+            this.buttonTriang.Click += new System.EventHandler(this.buttonTriang_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(673, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Triangles";
+            // 
+            // buttonThreeColor
+            // 
+            this.buttonThreeColor.Enabled = false;
+            this.buttonThreeColor.Location = new System.Drawing.Point(762, 105);
+            this.buttonThreeColor.Name = "buttonThreeColor";
+            this.buttonThreeColor.Size = new System.Drawing.Size(75, 25);
+            this.buttonThreeColor.TabIndex = 6;
+            this.buttonThreeColor.Text = "3-color";
+            this.buttonThreeColor.UseVisualStyleBackColor = true;
+            this.buttonThreeColor.Click += new System.EventHandler(this.buttonThreeColor_Click);
+            // 
+            // buttonArie
+            // 
+            this.buttonArie.Enabled = false;
+            this.buttonArie.Location = new System.Drawing.Point(762, 136);
+            this.buttonArie.Name = "buttonArie";
+            this.buttonArie.Size = new System.Drawing.Size(75, 25);
+            this.buttonArie.TabIndex = 7;
+            this.buttonArie.Text = "Get Area";
+            this.buttonArie.UseVisualStyleBackColor = true;
+            this.buttonArie.Click += new System.EventHandler(this.buttonArie_Click);
+            // 
+            // labelArie
+            // 
+            this.labelArie.AutoSize = true;
+            this.labelArie.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelArie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelArie.Location = new System.Drawing.Point(762, 172);
+            this.labelArie.Name = "labelArie";
+            this.labelArie.Size = new System.Drawing.Size(2, 18);
+            this.labelArie.TabIndex = 8;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(9, 390);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(251, 13);
+            this.labelInfo.TabIndex = 9;
+            this.labelInfo.Text = "Punctele se deseneaza in sens invers trigonometric!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(858, 424);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.labelArie);
+            this.Controls.Add(this.buttonArie);
+            this.Controls.Add(this.buttonThreeColor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonTriang);
+            this.Controls.Add(this.buttonFinishUp);
+            this.Controls.Add(this.buttonDrawMode);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1TriangOtectomie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonDrawMode;
+        private System.Windows.Forms.Button buttonFinishUp;
+        private System.Windows.Forms.Button buttonTriang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonThreeColor;
+        private System.Windows.Forms.Button buttonArie;
+        private System.Windows.Forms.Label labelArie;
+        private System.Windows.Forms.Label labelInfo;
     }
 }
+
 
--- HW08_Triangulare_Otectomie/Form1.cs
+++ HW08_Triangulare_Otectomie/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace HW08_Triangulare_Otectomie
 {
@@ -16,5 +17,308 @@ namespace HW08_Triangulare_Otectomie
         {
             InitializeComponent();
         }
+        #region DrawPolygon
+
+        Graphics g;
+        List<Point> points = new List<Point>(); //lista de puncte, vf poligonului
+        List<Tuple<int, int, int>> triangles = new List<Tuple<int, int, int>>(); //lista de triunghiuri
+        int contor = 0;
+        bool drawingMode = false;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            if (drawingMode)
+            {
+                Pen pen = new Pen(Color.Black, 3);
+                Point aux = new Point(e.X, e.Y);
+                Pen linie = new Pen(Color.DarkViolet, 2);
+                g.DrawString(contor.ToString(), new Font(FontFamily.GenericSansSerif, 10), new SolidBrush(Color.Black), aux.X - 20, aux.Y - 20);
+                contor++;
+                g.DrawEllipse(pen, aux.X - 2, aux.Y - 2, 4, 4);
+                if (points.Count != 0)
+                {
+                    g.DrawLine(linie, aux, points[points.Count - 1]);
+                }
+                points.Add(aux);
+            }
+        }
+
+        private void buttonDrawMode_Click(object sender, EventArgs e)
+        {
+            if (!drawingMode)
+            {
+                drawingMode = true;
+                buttonDrawMode.Enabled = false;
+                buttonFinishUp.Enabled = true;
+            }
+        }
+
+        private void buttonFinishUp_Click(object sender, EventArgs e)
+        {
+            if (drawingMode)
+            {
+                Graphics g = panel1.CreateGraphics();
+                Pen linie = new Pen(Color.DarkViolet, 2);
+                g.DrawLine(linie, points[0], points[points.Count - 1]);
+                buttonFinishUp.Enabled = false;
+                drawingMode = false;
+                buttonTriang.Enabled = true;
+            }
+        }
+        #endregion
+
+        #region TriangulareDiag
+
+        private void buttonTriang_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen penTR = new Pen(Color.Black, 2);
+            List<Point> puncteTriangulare = new List<Point>(points); //clona listei points
+            int n = puncteTriangulare.Count;
+            int newLabelY = label1.Location.Y + 20;
+            while (n > 3)
+            {
+                for (int i = 0; i < n - 2; i++)
+                {
+                    if (IsDiagonal(puncteTriangulare, i, i+2))
+                    {
+                        Thread.Sleep(100);
+                        IdentificareTriunghi(newLabelY, puncteTriangulare, i); //scrie in label triunghiul
+                        newLabelY += 20;
+                        g.DrawLine(penTR, puncteTriangulare[i], puncteTriangulare[i+2]); //deseneaza diagonala
+                        puncteTriangulare.RemoveAt(i + 1); //sterge varful urechii
+                        n--;
+                        break;
+                    }
+                }
+            }
+            IdentificareTriunghi(newLabelY, puncteTriangulare, 0); //scrie ultimul triunghi
+
+            buttonThreeColor.Enabled = true;
+            buttonArie.Enabled = true;
+        }
+        /// <summary>
+        /// Verifica daca o latura este diagonala.
+        /// Adica daca latura nu intersecteaza nicio latura neincidenta si daca este in interiorul poligonului
+        /// </summary>
+        /// <returns></returns>
+        private bool IsDiagonal(List<Point> puncte, int i, int j)
+        {
+            bool intersectie = false;
+            //nu intersecteaza laturi neinciente
+            for (int k = 0; k < puncte.Count - 1; k++)
+            {
+                if (i != k && i != (k + 1) && j != k && j != (k + 1) && se_intersecteaza(puncte[i], puncte[j], puncte[k], puncte[k + 1]))
+                {
+                    intersectie = true;
+                    break;
+                }
+            }
+            //vf pt ultima latura
+            if (i != puncte.Count - 1 && i != 0 && j != puncte.Count - 1 && j != 0 && se_intersecteaza(puncte[i], puncte[j], puncte[puncte.Count - 1], puncte[0]))
+            {
+                intersectie = true;
+            }
+            //se afla in interiorul P
+            if (!intersectie && se_afla_in_interiorul_poligonului(puncte, i, j))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Pune intr-un label triunghiurile triangulate
+        private void IdentificareTriunghi(int labelY, List<Point> puncteTriangulare, int i)
+        {
+            Label labelTriangle = new Label();
+            labelTriangle.Parent = panel1;
+            labelTriangle.Location = new Point(label1.Location.X, labelY);
+            labelTriangle.Text = "";
+            labelTriangle.AutoSize = true;
+            this.Controls.Add(labelTriangle);
+            int triangleV1 = 0, triangleV2 = 0, triangleV3 = 0;
+            for (int j = 0; j < points.Count; j++)
+            {
+                if (points[j].X == puncteTriangulare[i].X && points[j].Y == puncteTriangulare[i].Y)
+                {
+                    labelTriangle.Text = labelTriangle.Text + (j).ToString() + ", "; //primul vf al triunghiului
+                    triangleV1 = j;
+                    break;
+                }
+            }
+            for (int j = 0; j < points.Count; j++)
+            {
+                if (points[j].X == puncteTriangulare[i + 1].X && points[j].Y == puncteTriangulare[i + 1].Y)
+                {
+                    labelTriangle.Text = labelTriangle.Text + (j).ToString() + ", "; //al doilea vf al triunghiului
+                    triangleV2 = j;
+                    break;
+                }
+            }
+            for (int j = 0; j < points.Count; j++)
+            {
+                if (points[j].X == puncteTriangulare[i + 2].X && points[j].Y == puncteTriangulare[i + 2].Y)
+                {
+                    labelTriangle.Text = labelTriangle.Text + (j).ToString(); //si al treilea vf
+                    triangleV3 = j;
+                    break;
+                }
+            }
+            triangles.Add(new Tuple<int, int, int>(triangleV1, triangleV2, triangleV3));
+        }
+        #region FunctiiDiagonala
+        private int ValoareDeterminant(Point a, Point b, Point c)
+        {
+            return a.X * b.Y + b.X * c.Y + c.X * a.Y - c.X * b.Y - a.X * c.Y - b.X * a.Y;
+        }
+
+        private bool se_afla_in_interiorul_poligonului(List<Point> puncte, int pi, int pj)
+        {
+            int pi_ant = (pi > 0) ? pi - 1 : puncte.Count - 1;
+            int pi_urm = (pi < puncte.Count - 1) ? pi + 1 : 0;
+            if ((este_varf_convex(puncte, pi) && intoarcere_spre_stanga(puncte, pi, pj, pi_urm) && intoarcere_spre_stanga(puncte, pi, pi_ant, pj)) || (este_varf_reflex(puncte, pi) && !(intoarcere_spre_dreapta(puncte, pi, pj, pi_urm) && intoarcere_spre_dreapta(puncte, pi, pi_ant, pj))))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool intoarcere_spre_dreapta(List<Point> puncte, int p1, int p2, int p3)
+        {
+            if (ValoareDeterminant(puncte[p1], puncte[p2], puncte[p3]) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool intoarcere_spre_stanga(List<Point> puncte, int p1, int p2, int p3)
+        {
+            if (ValoareDeterminant(puncte[p1], puncte[p2], puncte[p3]) < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool este_varf_reflex(List<Point> puncte, int p)
+        {
+            int p_ant = (p > 0) ? p - 1 : puncte.Count - 1;
+            int p_urm = (p < puncte.Count - 1) ? p + 1 : 0;
+            return intoarcere_spre_stanga(puncte, p_ant, p, p_urm);
+        }
+
+        private bool este_varf_convex(List<Point> puncte, int p)
+        {
+            int p_ant = (p > 0) ? p - 1 : puncte.Count - 1;
+            int p_urm = (p < puncte.Count - 1) ? p + 1 : 0;
+            return intoarcere_spre_dreapta(puncte, p_ant, p, p_urm);
+        }
+
+        private bool se_intersecteaza(Point s1, Point s2, Point p1, Point p2)
+        {
+            if (ValoareDeterminant(p2, p1, s1) * ValoareDeterminant(p2, p1, s2) <= 0 && ValoareDeterminant(s2, s1, p1) * ValoareDeterminant(s2, s1, p2) <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #endregion
+
+        #region 3-Colorare
+
+        private void buttonThreeColor_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen []pens = new Pen[] { new Pen(Color.Blue, 3), new Pen(Color.Green, 3), new Pen(Color.Red, 3) };
+            List<Tuple<int, int>> varfuriMarcate = new List<Tuple<int, int>>();
+            for (int i = triangles.Count - 1; i >= 0; i--)
+            {
+                int colored1 = IsAlreadyMarked(triangles[i].Item1, varfuriMarcate);
+                int colored2 = IsAlreadyMarked(triangles[i].Item2, varfuriMarcate);
+                int colored3 = IsAlreadyMarked(triangles[i].Item3, varfuriMarcate);
+                if (colored1 == -1 && colored2 == -1 && colored3 == -1) //primul caz
+                {
+                    varfuriMarcate.Add(new Tuple<int, int>(triangles[i].Item1, 0));
+                    g.DrawEllipse(pens[0], points[triangles[i].Item1].X - 8, points[triangles[i].Item1].Y - 8, 16, 16);
+                    varfuriMarcate.Add(new Tuple<int, int>(triangles[i].Item2, 1));
+                    g.DrawEllipse(pens[1], points[triangles[i].Item2].X - 8, points[triangles[i].Item2].Y - 8, 16, 16);
+                    varfuriMarcate.Add(new Tuple<int, int>(triangles[i].Item3, 2));
+                    g.DrawEllipse(pens[2], points[triangles[i].Item3].X - 8, points[triangles[i].Item3].Y - 8, 16, 16);
+                }
+                else if(colored1 == -1)
+                {
+                    varfuriMarcate.Add(new Tuple<int, int>(triangles[i].Item1, MissingColor(colored2, colored3)));
+                    g.DrawEllipse(pens[MissingColor(colored2, colored3)], points[triangles[i].Item1].X - 8, points[triangles[i].Item1].Y - 8, 16, 16);
+                }
+                else if (colored2 == -1)
+                {
+                    varfuriMarcate.Add(new Tuple<int, int>(triangles[i].Item2, MissingColor(colored1, colored3)));
+                    g.DrawEllipse(pens[MissingColor(colored1, colored3)], points[triangles[i].Item2].X - 8, points[triangles[i].Item2].Y - 8, 16, 16);
+                }
+                else if (colored3 == -1)
+                {
+                    varfuriMarcate.Add(new Tuple<int, int>(triangles[i].Item3, MissingColor(colored1, colored2)));
+                    g.DrawEllipse(pens[MissingColor(colored1, colored2)], points[triangles[i].Item3].X - 8, points[triangles[i].Item3].Y - 8, 16, 16);
+                }
+            }
+        }
+
+        private int IsAlreadyMarked(int punct, List<Tuple<int, int>> varfuriMarcate) //verifica daca e marcat si returneaza culoarea
+        {
+            for (int i = 0; i < varfuriMarcate.Count; i++)
+            {
+                if (varfuriMarcate[i].Item1 == punct)
+                {
+                    return varfuriMarcate[i].Item2;
+                }
+            }
+            return -1;
+        }
+
+        private int MissingColor(int a, int b) //returneaza a treia culoare
+        {
+            if ((a == 0 && b == 1) || (a == 1 && b == 0))
+            {
+                return 2;
+            }
+            if((a == 0 && b == 2) || (a == 2 && b == 0))
+            {
+                return 1;
+            }
+            if((a == 1 && b == 2) || (a == 2 && b == 1))
+            {
+                return 0;
+            }
+            return -1;
+        }
+
+
+        #endregion
+
+        #region Arie
+        private void buttonArie_Click(object sender, EventArgs e) //calculeaza suma ariilor triunghiurilor
+        {
+            double arieTotala = 0;
+            for (int i = 0; i < triangles.Count; i++)
+            {
+                arieTotala += ArieTriunghi(points[triangles[i].Item1].X, points[triangles[i].Item1].Y, points[triangles[i].Item2].X, points[triangles[i].Item2].Y, points[triangles[i].Item3].X, points[triangles[i].Item3].Y);
+            }
+            labelArie.Text = arieTotala.ToString();
+        }
+
+        private double ArieTriunghi(int x1, int y1, int x2, int y2, int x3, int y3) //formula cu determinant
+        {
+            return 0.5 * Math.Abs(x1 * y2 + x2 * y3 + x3 * y1 - x3 * y2 - x1 * y3 - x2 * y1);
+        }
+        #endregion
     }
 }
